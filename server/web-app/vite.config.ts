74a515178a7ae63cd7083af8cc5e5bc095c40e05
Skye <166605393+fitzylala@users.noreply.github.com>
2026-05-26 15:17:22 +0100

    Fix: Allow requests from our host domain

--- server/web-app/vite.config.ts
+++ server/web-app/vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 80, host: true },
-  preview: { port: 80, host: true },
+  server: { port: 80, host: true, allowedHosts: ['1bit2qbit.theburkenator.com'] },
+  preview: { port: 80, host: true, allowedHosts: ['1bit2qbit.theburkenator.com'] },
 })
