f81ef3040cdab890754337894cb9b2af6d69975a
meltedelement <meltedelement@gmail.com>
2026-05-28 11:55:32 +0100

    fixed pyproject and changed title

--- server/backend/main.py
+++ server/backend/main.py
@@ -2,5 +2,5 @@ from fastapi import FastAPI
 
 from .routes import auth
 
-app = FastAPI(title="EPIC 1bit2qbit API", version="0.1.0")
+app = FastAPI(title="1bit2qbit", version="0.1.0")
 app.include_router(auth.router)
