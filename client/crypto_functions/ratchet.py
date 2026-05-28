d586ca8a675fdd956a658289c4637e47dab8b15e
Sark37 <aaronodoherty7@gmail.com>
2026-05-28 16:36:47 +0100

    renamed ratchet file to avoid module confusion

--- client/crypto_functions/ratchet.py
+++ client/crypto_functions/ratchet.py
@@ -2,8 +2,8 @@ import os
 from typing import Any, Dict
 
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
-from doubleratchet import DoubleRatchet as DR
-from doubleratchet import Header
+from client.crypto_functions.ratchet import DoubleRatchet as DR
+from client.crypto_functions.ratchet import Header
 from doubleratchet.recommended import HashFunction
 from doubleratchet.recommended import diffie_hellman_ratchet_curve25519 as dhr25519
 from doubleratchet.recommended import kdf_hkdf, kdf_separate_hmacs
