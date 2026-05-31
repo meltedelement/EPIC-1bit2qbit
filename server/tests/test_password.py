010b4f563a519c0a2800e85ea37d9b8df3dbf547
Sark37 <aaronodoherty7@gmail.com>
2026-05-31 12:37:33 +0100

    added tests for argon2id password hashing

---
+++ server/tests/test_password.py
@@ -0,0 +1,42 @@
+import pytest
+from argon2.exceptions import VerifyMismatchError
+
+from backend.crypto.password import hash_password, needs_rehash, verify_password
+
+_PASSWORD = "correct-horse-battery-staple"
+
+
+class TestHashPassword:
+    def test_returns_string(self):
+        assert isinstance(hash_password(_PASSWORD), str)
+
+    def test_output_is_argon2id(self):
+        assert hash_password(_PASSWORD).startswith("$argon2id$")
+
+    def test_unique_hashes(self):
+        assert hash_password(_PASSWORD) != hash_password(_PASSWORD)
+
+
+class TestVerifyPassword:
+    def test_correct_password_returns_true(self):
+        hashed = hash_password(_PASSWORD)
+        assert verify_password(_PASSWORD, hashed) is True
+
+    def test_wrong_password_returns_false(self):
+        hashed = hash_password(_PASSWORD)
+        assert verify_password("wrong-password", hashed) is False
+
+    def test_empty_password_returns_false(self):
+        hashed = hash_password(_PASSWORD)
+        assert verify_password("", hashed) is False
+
+
+class TestNeedsRehash:
+    def test_fresh_hash_does_not_need_rehash(self):
+        assert needs_rehash(hash_password(_PASSWORD)) is False
+
+    def test_outdated_parameters_needs_rehash(self):
+        from argon2 import PasswordHasher
+        old_ph = PasswordHasher(time_cost=1, memory_cost=8, parallelism=1)
+        old_hash = old_ph.hash(_PASSWORD)
+        assert needs_rehash(old_hash) is True
