e89574faeedb23360cda9bf02235d61fe5e4f4da
Skye <166605393+fitzylala@users.noreply.github.com>
2026-05-31 16:22:05 +0100

    Feat: Cryptography key management and x3DH

---
+++ client/crypto_functions/__init__.py
@@ -0,0 +1,13 @@
+from crypto_functions.dek import create_dek, rotate_dek, unlock_dek
+from crypto_functions.x3dh_init import create_state
+from crypto_functions.ratchet import AES256GCMAEAD, DoubleRatchet, dr_configuration
+
+__all__ = [
+    "create_dek",
+    "unlock_dek",
+    "rotate_dek",
+    "create_state",
+    "AES256GCMAEAD",
+    "DoubleRatchet",
+    "dr_configuration",
+]
